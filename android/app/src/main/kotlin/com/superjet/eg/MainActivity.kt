5c88dde393f707eef4edda7ed86cda8e76d9c0a0
Yahia <Yehyaelzaeem>
2023-10-02 23:14:59 +0100

    push the second part of admin xx

---
+++ android/app/src/main/kotlin/com/superjet/eg/MainActivity.kt
@@ -0,0 +1,8 @@
+package com.superjet.eg
+
+import io.flutter.embedding.android.FlutterActivity
+
+import io.flutter.embedding.android.FlutterFragmentActivity
+
+class MainActivity: FlutterFragmentActivity() {
+}
