4cc6d5fdb7de830a9a48c7bb6b66add865715b8f
Yahia <Yehyaelzaeem>
2023-11-28 18:13:51 +0000

    the last push2 in first version

--- lib/super_jet_app/onBoarding/presentation/screens/onboarding_screen.dart
+++ lib/super_jet_app/onBoarding/presentation/screens/onboarding_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:hexcolor/hexcolor.dart';
 import 'package:smooth_page_indicator/smooth_page_indicator.dart';
 import '../bloc/cubit.dart';
 import '../bloc/states.dart';
